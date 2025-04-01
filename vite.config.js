40c80dc03900288505176d07bc06f7aab23823f3
helloakshay27 <helloakshay27@gmail.com>
2025-04-01 13:54:38 +0530

    remove Vercel configuration and simplify Vite config by adding React plugin

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  base: './', // Ensures relative paths for assets
-  build: {
-    outDir: 'dist',
-  },
-});
+  plugins: [react()],
+})
