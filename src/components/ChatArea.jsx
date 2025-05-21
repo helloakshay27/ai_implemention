bc3d7de0d0411208fa7431f55124889700f18984
atharv0811 <karnekaratharv12@gmail.com>
2025-05-21 17:16:25 +0530

    Refactor WebSocket connection setup in ChatArea and remove HMR protocol configuration from Vite

--- src/components/ChatArea.jsx
+++ src/components/ChatArea.jsx
@@ -37,10 +37,7 @@ const ChatArea = () => {
     };
 
     useEffect(() => {
-        // wsRef.current = new WebSocket(`ws://43.204.42.235:8000/ws/chat/${id}/`);
-        const socket = new WebSocket(`ws://43.204.42.235:8000/ws/chat/${id}/`);
-
-        wsRef.current = socket;
+        wsRef.current = new WebSocket(`ws://43.204.42.235:8000/ws/chat/${id}/`);
 
         wsRef.current.onopen = () => {
             console.log("WebSocket connection established");
